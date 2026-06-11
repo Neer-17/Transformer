df21d8b31df6f65c5e322ecdd708dd0c6dd8963e
neeraj <neerajpanwar844@gmail.com>
2026-06-11 14:50:37 +0530

    Fixed Bugs

--- src/model/attention.py
+++ src/model/attention.py
@@ -14,10 +14,10 @@ def scaled_dot_product_attention(query:torch.Tensor, key:torch.Tensor, value:tor
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self):
+    def __init__(self,d_model:int,num_heads:int):
         super().__init__()
-        self.d_model = 256
-        self.num_heads = 8
+        self.d_model = d_model
+        self.num_heads = num_heads
         self.d_k = self.d_model // self.num_heads
         self.W_q = nn.Linear(self.d_model,self.d_model)
         self.W_k = nn.Linear(self.d_model,self.d_model)
@@ -32,22 +32,22 @@ class MultiHeadAttention(nn.Module):
         K = self.W_k(x)
         V = self.W_v(x)
 
-        Q = Q.view(Q.size(0),Q.size(1),self.num_heads,self.d_k)
-        K = K.view(K.size(0),K.size(1),self.num_heads,self.d_k)
-        V = V.view(V.size(0),V.size(1),self.num_heads,self.d_k)
+        Q = Q.view(batch_size,seq_len,self.num_heads,self.d_k)
+        K = K.view(batch_size,seq_len,self.num_heads,self.d_k)
+        V = V.view(batch_size,seq_len,self.num_heads,self.d_k)
 
         Q = Q.transpose(1,2)
         K = K.transpose(1,2)
         V = V.transpose(1,2)
 
 
-        weights,output = scaled_dot_product_attention(Q,K,V,mask=True)
+        weights,output = scaled_dot_product_attention(Q,K,V,mask=mask)
 
         output = output.transpose(1,2).contiguous()
         output = output.view(batch_size,seq_len,self.d_model)
 
         final_output = self.W_o(output)
-        return final_output
+        return weights,final_output
     
 
             
